dcbd7ce00d9bde0a025c59862fb7e9e79e3f0e3a
Don Coleman <dcoleman@chariotsolutions.com>
2012-07-25 20:44:53 -0400

    fix plugin name

--- src/wp7/Hello.cs
+++ src/wp7/Hello.cs
@@ -4,14 +4,13 @@ using WP7CordovaClassLib.Cordova.JSON;
 
 namespace Cordova.Extension.Commands
 {
-    public class HelloPlugin : BaseCommand
+    public class Hello : BaseCommand
     {
         public void greet(string args)
         {
             string name = JsonHelper.Deserialize<string>(args);
             string message = "Hello, " + name;
-            PluginResult result = 
-                new PluginResult(PluginResult.Status.OK, message);
+            PluginResult result = new PluginResult(PluginResult.Status.OK, message);
             DispatchCommandResult(result);
         }
     }
