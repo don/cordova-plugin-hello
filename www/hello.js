6e83f60212180dda1164a23b8770cd5f06c8e296
Don Coleman <dcoleman@chariotsolutions.com>
2014-12-19 11:33:24 -0500

    style

--- www/hello.js
+++ www/hello.js
@@ -1,11 +1,7 @@
-/*global cordova*/
+/*global cordova, module*/
 
-var exec = require("cordova/exec");
-
-var Hello = function(){};
-
-Hello.prototype.greet = function (name, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+module.exports = {
+    greet: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+    }
 };
-
-module.exports = new Hello();
