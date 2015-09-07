1741443d8890aae4bd46c8c4f2ac5fc6c35cfee8
Maxwell <maxwellsouzacarvalho@gmail.com>
2015-09-07 01:00:53 -0300

    Matches default documentation and added comments
    Changed to math with the default documentation, for the beginners know how to have multiple methods.
    added comments to easy explain beginner users what is going on.
    only a few comments, if its a good idea, more could be added.

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -5,22 +5,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class Hello extends CordovaPlugin {
-
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
+            /*if() to select what method of the class should be used.
+            *on this class, we have only the greet method
+            */
         if (action.equals("greet")) {
-
             String name = data.getString(0);
+            this.greet(name, callbackContext);
+            return true;
+        } else {
+            return false;
+        }
+        //the method return the String message to the sucessCallback(sucess function added on index.js)
+        private void greet(String name, CallbackContext callbackContext){
             String message = "Hello, " + name;
             callbackContext.success(message);
-
-            return true;
-
-        } else {
-            
-            return false;
-
         }
     }
 }
