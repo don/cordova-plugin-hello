9e2d5315cd994d2d622bf66516de09e3ff745137
KoenLav <k.lavrijssen@gmail.com>
2017-10-07 15:33:17 +0200

    Updated SDK

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -13,43 +13,27 @@ import com.mrwinston.icardpossdk.POSHandler;
 import com.mrwinston.icardpossdk.TransactionData;
 
 public class Hello extends CordovaPlugin {
-    private static final int    REQUEST_CODE_MAKE_PAYMENT  = 1;
-
     private POSHandler mPOSHandler;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
-        final CordovaPlugin that = this;
-
         if (action.equals("greet")) {
             POSHandler.setCurrency(Currency.EUR);
             mPOSHandler = POSHandler.getInstance();
 
+            BluetoothDevicesDialog dialog = new BluetoothDevicesDialog(this);
+            dialog.show();
+
             mPOSHandler.setConnectionListener(new ConnectionListener() {
                 @Override
                 public void onConnected(final BluetoothDevice device) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            mStatus.setText("Connected");
-
-                            mTerminalType.setVisibility(View.VISIBLE);
-
-                            mTerminalType.setText(device.getName().equalsIgnoreCase("") ? device.getAddress() : device.getName());
-
-                            setEnabled(true);
-
-                            if( POSHandler.getInstance().isConnected()){
-                                mPOSHandler.openPaymentActivity(
-                                        that.cordova.getActivity(),
-                                        REQUEST_CODE_MAKE_PAYMENT /*requestCode*/,
-                                        "12.12",
-                                        UUID.randomUUID().toString()
-                                );
-                            }
-                        }
-                    });
+                    if( POSHandler.getInstance().isConnected()){
+                        mPOSHandler.purchase(
+                            "12.12",
+                            UUID.randomUUID().toString() /*transaction reference*/,
+                            POSHandler.RECEIPT_PRINT_AUTOMATICALLY /*receipt configuration*/
+                        );
+                    }
                 }
             });
 
