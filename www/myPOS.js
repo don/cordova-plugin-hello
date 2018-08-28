f0d422e9fb2d59e6fe34d7b0e0e0bb61b94457b2
Koen Lavrijssen <k.lavrijssen@gmail.com>
2018-08-28 15:29:20 +0200

    Updated function signature

--- www/myPOS.js
+++ www/myPOS.js
@@ -1,7 +1,7 @@
 /*global cordova, module*/
 
 module.exports = {
-    payment: function (amount, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "myPOS", "payment", [amount]);
+    payment: function (amount, connectionType, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "myPOS", "payment", [amount, connectionType]);
     }
 };
