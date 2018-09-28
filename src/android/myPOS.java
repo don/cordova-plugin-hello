6b87865d9f9762b5177626c2bd725a55e5017221
Koen Lavrijssen <k.lavrijssen@gmail.com>
2018-09-28 16:33:30 +0200

    mypos-beta: Updated

--- src/android/myPOS.java
+++ src/android/myPOS.java
@@ -36,6 +36,9 @@ public class myPOS extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, final JSONArray data, final CallbackContext callbackContext) throws JSONException {
+        // Set the callback for the activity result to this class
+        cordova.setActivityResultCallback(myPOS.this);
+
         this.callbackContext = callbackContext;
 
         if (action.equals("payment")) {
@@ -50,9 +53,6 @@ public class myPOS extends CordovaPlugin {
             POSHandler.setDefaultReceiptConfig(POSHandler.RECEIPT_PRINT_ONLY_MERCHANT_COPY);
 
             mPOSHandler = POSHandler.getInstance();
-            
-            // Set the callback for the activity result to this class
-            cordova.setActivityResultCallback(myPOS.this);
 
             if( mPOSHandler.isConnected() ) {
                 // We are already connected, start the payment
@@ -83,6 +83,18 @@ public class myPOS extends CordovaPlugin {
     }
 
     private void paymentViaActivityThread(final JSONArray data) {
+        mPOSHandler.setPOSInfoListener(new POSInfoListener() {
+            @Override
+            public void onPOSInfoReceived(final int command, final int status, final String description) {
+                // Handle the response here
+            }
+        
+            @Override
+            public void onTransactionComplete(final TransactionData transactionData) {
+                callbackContext.success();
+            }
+        });
+
         Thread thread = new Thread(new Runnable() {
             public void run() {
                 paymentViaActivity(data);
@@ -132,21 +144,17 @@ public class myPOS extends CordovaPlugin {
         );
     }
 
-    // TODO: find out why this doesn't work sometimes... or just the first time after switching connection methods?
-    // TODO: implement mPOSHandler.setPOSInfoListener?
-    @Override
-    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        activity.runOnUiThread(new Runnable() {
-            public void run() {
-                if( requestCode == REQUEST_CODE_MAKE_PAYMENT && resultCode == RESULT_OK) {
-                    callbackContext.success();
-                }
-                else {
-                    callbackContext.error(resultCode);
-                }
-            }
-        });
-    }
+    // // TODO: find out why this doesn't work sometimes... or just the first time after switching connection methods?
+    // // TODO: implement mPOSHandler.setPOSInfoListener?
+    // @Override
+    // public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+    //     if( requestCode == REQUEST_CODE_MAKE_PAYMENT && resultCode == RESULT_OK) {
+    //         callbackContext.success();
+    //     }
+    //     else {
+    //         callbackContext.error(resultCode);
+    //     }
+    // }
 
     private void toast(final String message) {
         activity.runOnUiThread(new Runnable() {
